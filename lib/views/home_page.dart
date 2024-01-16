2fbc70ef3f1baa7c176d6db581eb39e1205049ee
Haseeb1a <mh3824555@gmail.com>
2024-01-16 23:34:23 +0530

    first commit

---
+++ lib/views/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        title: const Text(
+          'ChatApp',
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.normal, fontSize: 19),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.comment_bank_rounded),
+        ),
+      ),
+    );
+  }
+}
